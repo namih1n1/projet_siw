c0b40354808e2bf63f6b859fb70ebc9dc1e46e0c
namih1n1 <alexandre.hoainam.le@gmail.com>
2015-01-06 13:50:08 +0100

    Update actorsByFilm.php

--- actorsByFilm.php
+++ actorsByFilm.php
@@ -1,3 +1,13 @@
 <?php
 include("./includes/header.php");
+
+
+select ?titres ?actor where {
+?Resource dbpedia-owl:starring ?actor ;
+rdfs:label ?titres .
+FILTER langmatches(lang(?titres),"fr") .
+}
+ORDER BY ?titres ?actor
+
+
 ?>
