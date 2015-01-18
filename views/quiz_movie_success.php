77faab046873ae886937a537016bee8d7547e073
Elsa <elsa.samimi@outlook.com>
2015-01-18 17:12:30 +0100

    css quizz films

--- views/quiz_movie_success.php
+++ views/quiz_movie_success.php
@@ -13,16 +13,21 @@ include("../includes/header.php");
 
 	<?php 
 	echo "
+		<div class = \"quizz\">
 		<p>Est-ce que \"" . utf8_decode($quizmovie[0]['sm_titre']) . "\" est sorti avant \"" . utf8_decode($quizmovie[1]['sm_titre']) . "\" ?</p>
 		<input id=\"answer_no\" type=\"button\" name=\"no\" value=\"NON\" onclick=\"answer_no(". $quizmovie[0]['sm_annee'] .",". $quizmovie[1]['sm_annee'] .")\" />
 		<input id=\"answer_yes\" type=\"button\" name=\"yes\" value=\"OUI\" onclick=\"answer_yes(". $quizmovie[0]['sm_annee'] .",". $quizmovie[1]['sm_annee'] .")\" />
-		
+		</div>
+		<div class=\"propositions\">
 		<div class=\"reponse_quiz\"></div>
 		
 		<div id=\"choix\" style=\"display:none\">
 			<input type=\"button\" name=\"again\" value=\"SUIVANT\" onclick=\"again()\"  />
 			<input type=\"button\" name=\"other\" value=\"Autre quiz\" onclick=\"changerQuiz()\" />
-		</div>";
+		</div>
+		</div>
+		
+		";
 
 echo "<script language=\"javascript\">
 var juste 	= \"Vous avez raison !\";
