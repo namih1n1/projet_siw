cec443b66a41e5991a90f4ec628157fd89809c88
namih1n1 <alexandre.hoainam.le@gmail.com>
2015-02-06 00:19:53 +0100

    Derniers commits avant présentation oral

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 		<h4>Site cr&eacute;&eacute; par Elsa SAMIMI et Alexandre LE<br />
 		dans le cadre du projet WEB-SIW de 2&egrave;me ann&eacute;e au CNAM (2014-2015)
 		</h4>
-		<h4>Contactez-nous : <a href="mailto:elsa.samimi@gmail.com">E.SAMIMI</a> OU <a class="blink" href="mailto:alexandre.hoainam.le@gmail.com">A.LE</a></h4>
+		<h4>Contactez-nous : <a href="mailto:elsa.samimi@gmail.com">E.SAMIMI</a> OU <a href="mailto:alexandre.hoainam.le@gmail.com">A.LE</a></h4>
 		<h4>HTML 5 - CSS 3</h4>
 	</div>
 	</body>
